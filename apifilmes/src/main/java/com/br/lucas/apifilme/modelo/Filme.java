ee3d2de5148217360d1fac9f1cf18c27ad1fe594
Lucas Caio Vargas <lucascaio9163@gmail.com>
2022-01-27 21:06:40 -0300

    Iniciando o projeto

---
+++ apifilmes/src/main/java/com/br/lucas/apifilme/modelo/Filme.java
@@ -0,0 +1,8 @@
+package com.br.lucas.apifilme.modelo;
+
+public class Filme {
+	public static void main(String[] args) {
+		System.out.println("mfdmdfmdkmfdkm");
+		System.out.println("kfmakm");
+	}
+}
---
+++ apifilmes/src/test/java/com/br/lucas/apifilme/ApifilmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.lucas.apifilme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifilmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
